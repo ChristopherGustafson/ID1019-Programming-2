8e80b007e92ba649ee78c3118ee257747a2ca466
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-02-14 19:28:03 +0100

    Added more effective mandelbrot

--- Mandelbrot/cmplx.ex
+++ Mandelbrot/cmplx.ex
@@ -20,4 +20,24 @@ defmodule Cmplx do
     :math.sqrt(r*r+i*i)
   end
 
+  def mandelbrot({r, i}, m) do
+    zr = 0
+    zi = 0
+    test(0, zr, zi, r, i, m)
+  end
+  defp test(m, _, _, _, _, m) do 0 end
+  defp test(i, zr, zi, cr, ci, m) do
+    zr2 = zr * zr
+    zi2 = zi * zi
+    a2 = zr2 + zi2
+
+    if(a2 < 4.0) do
+      sr = zr2 - zi2 + cr
+      si = 2*zr*zi + ci
+      test(i+1, sr, si, cr, ci, m)
+    else
+      i
+    end
+  end
+
 end
